92556538032d9dba8cf236cac982fe3b87e9b987
smc9759 <42694805+smc9759@users.noreply.github.com>
2023-08-29 10:06:13 +0900

    add button

--- checkbox.cpp
+++ checkbox.cpp
@@ -52,7 +52,29 @@ LRESULT CALLBACK WndProc(HWND hWnd,UINT iMessage,WPARAM wParam,LPARAM lParam)
 	HBRUSH MyBrush, OldBrush;
 	switch(iMessage) {
 	case WM_CREATE:
-		return 0;
+		CreateWindow("button","Graph",WS_CHILD | WS_VISIBLE | 
+			BS_GROUPBOX,5,5,120,110,hWnd,(HMENU)0,g_hInst,NULL);
+		CreateWindow("button","Color",WS_CHILD | WS_VISIBLE | 
+			BS_GROUPBOX,145,5,120,110,hWnd,(HMENU)1,g_hInst,NULL);
+		r1=CreateWindow("button","Rectangle",WS_CHILD | WS_VISIBLE | 
+			BS_AUTORADIOBUTTON | WS_GROUP,
+			10,20,100,30,hWnd,(HMENU)ID_R1,g_hInst,NULL);
+		r2=CreateWindow("button","Ellipse",WS_CHILD | WS_VISIBLE | 
+			BS_AUTORADIOBUTTON,
+			10,50,100,30,hWnd,(HMENU)ID_R2,g_hInst,NULL);
+		r3=CreateWindow("button","Line",WS_CHILD | WS_VISIBLE | 
+			BS_AUTORADIOBUTTON,
+			10,80,100,30,hWnd,(HMENU)ID_R3,g_hInst,NULL);
+		r4=CreateWindow("button","Black",WS_CHILD | WS_VISIBLE | 
+			BS_AUTORADIOBUTTON | WS_GROUP,
+			150,20,100,30,hWnd,(HMENU)ID_R4,g_hInst,NULL);
+		r5=CreateWindow("button","Red",WS_CHILD | WS_VISIBLE | 
+			BS_AUTORADIOBUTTON,
+			150,50,100,30,hWnd,(HMENU)ID_R5,g_hInst,NULL);
+		r6=CreateWindow("button","Blue",WS_CHILD | WS_VISIBLE | 
+			BS_AUTORADIOBUTTON,
+			150,80,100,30,hWnd,(HMENU)ID_R6,g_hInst,NULL);
+		break;
 	case WM_PAINT:
 		hdc=BeginPaint(hWnd, &ps);
 		EndPaint(hWnd, &ps);
